c89793a7d4a504be05b8ab35a14d24bf19a233dd
HoangNhat2004 <nhnhat202@gmail.com>
2024-11-22 12:51:32 +0700

    Created home page components

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 /fontend:
       npm install
-      npm instal react-router-dom
-      npm install toastify
+      npm install react-router-dom react-toastify 
+      npm run dev(to run frontend)
+      npm install -D tailwindcss postcss autoprefixer
+      npx tailwindcss init -p
+Install extension: ES7+ React/Redux/React-Native snippets
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
-import Collection from './pages/collection'
-import About from './pages/about'
-import Contact from './pages/contact'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
         <Route path='/orders' element={<Orders/>} />
 
       </Routes>
+      <Footer/>
     </div>
   )
 }
