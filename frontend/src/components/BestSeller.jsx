45770dad0cc1fbbf77d104f4ba641af21a531323
HoangNhat2004 <nhnhat202@gmail.com>
2024-11-25 00:03:17 +0700

    About and contact page has been created

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -4,15 +4,14 @@ import Title from './Title'
 import ProductItem from './ProductItem'
 const BestSeller = () => {
   const {products} = useContext(ShopContext)
-  const {bestSeller, setBestSeller} = useState([])
+  const [bestSeller, setBestSeller] = useState([])
   useEffect(() => {
     const bestProduct = products.filter((item)=>(item.bestseller))
     setBestSeller(bestProduct.slice(0,5))
   },[])
-}
-return (
+  return (
     <div className='my-10'>
-        <div className='text-center text-3x1 py-8'>
+        <div className='text-center text-3xl py-8'>
             <Title text1={'BEST'} text2={'SELLERS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quis illum neque similique minima voluptas corporis nulla consequuntur. Accusantium sequi quis labore doloremque officiis obcaecati, eligendi illum at aliquid architecto?
@@ -27,4 +26,6 @@ return (
         </div>
     </div>
 )
+}
+
 export default BestSeller
