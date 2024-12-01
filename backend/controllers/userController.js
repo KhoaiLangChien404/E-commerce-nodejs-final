fcb33e3cc9a165418fef67a69c4878884948c329
HoangNhat2004 <nhnhat202@gmail.com>
2024-12-01 16:06:21 +0700

    Correct spelling errors

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,11 +62,11 @@ const adminLogin = async(req,res) => {
         if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD)
         {
             const token = jwt.sign(email+password, process.env.JWT_SECRET)
-            res.json({sucess:true, token})
+            res.json({success:true, token})
         }
         else
         {
-            res.json({sucess:false, message:"Invalid credentials"})
+            res.json({success:false, message:"Invalid credentials"})
         }
 
     } catch (error) {
