b8d679e212e151d59888df639a509b1b85e0dbf5
HoangNhat2004 <nhnhat202@gmail.com>
2024-12-08 12:15:48 +0700

    Resolve merging conflict

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,5 @@
 import express from 'express'
-<<<<<<< HEAD
 import { addProduct, listProducts, removeProduct, singleProduct, productStats } from '../controllers/productController.js'
-=======
-import { addProduct, listProducts, removeProduct, singleProduct } from '../controllers/productController.js'
->>>>>>> cf2e1869b557e3969b924c5377464f77434d9c79
 import upload from '../middleware/multer.js'
 import adminAuth from '../middleware/adminAuth.js'
 
@@ -13,9 +9,6 @@ productRouter.post('/add',adminAuth, upload.fields([{name:'image1',maxCount:1},{
 productRouter.post('/remove',adminAuth, removeProduct)
 productRouter.post('/single',singleProduct)
 productRouter.get('/list',listProducts)
-<<<<<<< HEAD
 productRouter.get('/stats', adminAuth, productStats)
-=======
->>>>>>> cf2e1869b557e3969b924c5377464f77434d9c79
 
 export default productRouter 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,22 +1,15 @@
 import express from 'express'
-<<<<<<< HEAD
 import { loginUser,registerUser,adminLogin,getUserProfile } from '../controllers/userController.js'
 import adminAuth from '../middleware/adminAuth.js';
 import { userStats } from '../controllers/userController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
-=======
-import { loginUser,registerUser,adminLogin } from '../controllers/userController.js'
->>>>>>> cf2e1869b557e3969b924c5377464f77434d9c79
 
 const userRouter = express.Router()
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 userRouter.post('/admin',adminLogin)
-<<<<<<< HEAD
 userRouter.get('/profile', authMiddleware, getUserProfile)
 userRouter.get('/stats', adminAuth, userStats);
-=======
->>>>>>> cf2e1869b557e3969b924c5377464f77434d9c79
 
 export default userRouter
